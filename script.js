0b4aebfe71083c92c8ac93ad9f00b166380160a0
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-05-09 19:02:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,8 @@ function fibonacci(num) {
         return 1;
     }
 
-    // Initialize variables to store the two previous Fibonacci numbers
-    let prevPrev = 0; // Initialize the first Fibonacci number
-    let prev = 1;     // Initialize the second Fibonacci number
-
-    // Iterate from the third Fibonacci number up to the numth Fibonacci number
-    for (let i = 2; i <= num; i++) {
-        // Calculate the current Fibonacci number by adding the previous two Fibonacci numbers
-        let current = prevPrev + prev;
-
-        // Update the previous two Fibonacci numbers for the next iteration
-        prevPrev = prev;
-        prev = current;
-    }
-    // Return the numth Fibonacci number
-    return prev;
+    // Recursive case: Fibonacci number is the sum of the previous two Fibonacci numbers
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 
